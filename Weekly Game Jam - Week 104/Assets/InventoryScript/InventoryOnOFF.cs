2783af5d55b27efe6575304a62b4eaada87f72e5
adieboy <capilitanadie@gmail.com>
2019-07-07 23:26:27 +0800

    Simple Inventory System

---
+++ Weekly Game Jam - Week 104/Assets/InventoryScript/InventoryOnOFF.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryOnOFF : MonoBehaviour
+{
+
+    public GameObject Inventory;
+    public int status;
+    public PlayerController myPlayer;
+    public CameraController myCam;
+
+
+    private void Start()
+    {
+      
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if(status == 0)// off screan
+            {
+                status = 1;
+                Inventory.SetActive(true);
+                myPlayer.enabled = false;
+                myCam.enabled = false;
+            }
+            else
+            {
+                Inventory.SetActive(false);
+                status = 0;
+                myPlayer.enabled = true;
+                myCam.enabled = true;
+
+            }
+        }
+        
+    }
+}
