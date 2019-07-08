816d7129fc08377ec6d06394c02c87073aee19ca
ioane37 <feiqrishviliioane@gmail.com>
2019-07-08 11:59:00 +0400

    Adding Enemy AI!

---
+++ Weekly Game Jam - Week 104/Assets/EnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform player = null;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void FindPlayer()
+    {
+
+    }
+
+    private void ChasePlayer()
+    {
+
+    }
+
+    private void AttackPlayer()
+    {
+
+    }
+
+    private void Patrol()
+    {
+
+    }
+}
