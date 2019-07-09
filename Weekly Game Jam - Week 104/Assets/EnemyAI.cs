8938c05ce048f323e495c059a85bfd6c6964a886
ioane37 <feiqrishviliioane@gmail.com>
2019-07-09 22:25:53 +0400

    Improved AI! 90% Done
    Now our AI is smarter.(Also Can Check Generator)

--- Weekly Game Jam - Week 104/Assets/EnemyAI.cs
+++ Weekly Game Jam - Week 104/Assets/EnemyAI.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using System;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float chaseRadius = 5f;
     [SerializeField] float checkRadius = 2f;
 
-    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] float rotationSpeed = 10f;
 
     [SerializeField] float damageAmount = 30f;
     [SerializeField] float damageCooldown = 2f;
@@ -17,6 +18,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float dwellDuration = 1.5f;
 
     Transform patrolArea = null;
+    Transform generator = null;
 
     NavMeshAgent agent = null;
 
@@ -24,16 +26,19 @@ public class EnemyAI : MonoBehaviour
 
     float currentDamageCooldown;
 
-    bool foundPlayer = false;
-    bool inChase = false;
-
     int patrolPointIndex = 0;
 
-    bool canPatrol = true;
+    RaycastHit hit; //TODO Use this to loose player
 
-    bool coroutineRunning = false;
+    enum State
+    {
+        Patrolling,
+        Chasing,
+        Attacking,
+        Checking
+    };
 
-    bool canCheckGenerator = false;
+    State state = State.Patrolling;
 
     void Awake()
     {
@@ -42,6 +47,7 @@ public class EnemyAI : MonoBehaviour
         player = FindObjectOfType<PlayerHealth>();
 
         patrolArea = GameObject.FindWithTag("Patrol Area").transform;
+        generator = GameObject.FindWithTag("Generator").transform;
 
         currentDamageCooldown = damageCooldown;
     }
@@ -50,44 +56,117 @@ public class EnemyAI : MonoBehaviour
     {
         FindPlayer();
 
-        if (foundPlayer)
-            inChase = true;
+        if (hit.transform != null)
+            Debug.Log(hit.transform.name);
 
-        if(inChase && Vector3.Distance(transform.position, player.transform.position) <= chaseRadius)
+        if (state == State.Patrolling)
         {
-            if (!foundPlayer)
-            {
-                transform.rotation = Quaternion.RotateTowards(
-                                     transform.rotation,
-                                     Quaternion.LookRotation(player.transform.position - transform.position),
-                                     rotationSpeed * Time.deltaTime
-                                     );
-            }
-
-            agent.SetDestination(player.transform.position);
+            StartCoroutine(Patroll());
+        }
+        else if (state == State.Chasing)
+        {
+            Chase();
+        }
+        else if (state == State.Attacking)
+        {
+            Attack();
         }
         else
         {
-            inChase = false;
+            CheckGenerator();
         }
+    }
 
-        //Patrol
-        if (!foundPlayer && !inChase && canPatrol)
+    private void CheckGenerator()
+    {
+        FaceTo(generator.position);
+
+        //TODO Playe Check Animation
+        if (FacingTo(generator.position))
         {
-            if (!coroutineRunning && Vector3.Distance(transform.position, patrolArea.GetChild(patrolPointIndex).position) <= agent.stoppingDistance)
-            {
-                StartCoroutine(Dwell());
-            }
+            state = State.Patrolling;
 
-            agent.SetDestination(patrolArea.GetChild(GetTargetPatrolPointIndex()).position);
+            //OR Chasing or Attacking
         }
 
-        if (Vector3.Distance(transform.position, player.transform.position) <= attackRadius && foundPlayer &&
-            player.Alive && currentDamageCooldown - Time.time < 0)
+        CheckSetState();
+    }
+
+    private void FaceTo(Vector3 targetPos)
+    {
+        Quaternion targetRot = transform.rotation;
+        targetRot.y = Quaternion.LookRotation(targetPos - transform.position).y;
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRot,
+            rotationSpeed * Time.deltaTime
+            );
+    }
+
+    private void CheckSetState()
+    {
+        float distanceBetweeenPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceBetweeenPlayer <= chaseRadius && FacingTo(player.transform.position))
+        {
+            state = State.Chasing;
+        }
+
+        if (distanceBetweeenPlayer <= attackRadius && FacingTo(player.transform.position))
+        {
+            state = State.Attacking;
+        }
+    }
+
+    private void Attack()
+    {
+        float distanceBetweeenPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        bool canAttack = currentDamageCooldown - Time.time < 0 &&
+                         player.Alive &&
+                         distanceBetweeenPlayer <= attackRadius;
+
+        bool facingToPlayer = FacingTo(player.transform.position);
+
+        if (canAttack && facingToPlayer)
         {
             player.TakeDamage(damageAmount);
 
             currentDamageCooldown = Time.time + damageCooldown;
+        }
+        else if (!facingToPlayer)
+        {
+            FaceTo(player.transform.position);
+        }
+
+        if (distanceBetweeenPlayer <= chaseRadius)
+        {
+            state = State.Chasing;
+        }
+        else if (!FacingTo(player.transform.position))
+        {
+            state = State.Patrolling;
+        }
+    }
+
+    private bool FacingTo(Vector3 targetPos)
+    {
+        return Vector3.Dot(transform.forward, (targetPos - transform.position).normalized) > 0.7f;
+    }
+
+    private bool FindPlayer()
+    {
+        return Physics.BoxCast(transform.position - Vector3.one, Vector3.one, transform.forward, out hit, Quaternion.identity, viewDistance);
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= attackRadius)
+        {
+            state = State.Attacking;
         }
     }
 
@@ -108,20 +187,22 @@ public class EnemyAI : MonoBehaviour
         return patrolPointIndex++;
     }
 
-    private void FindPlayer()
+    private IEnumerator Patroll()
     {
-        foundPlayer = Physics.BoxCast(transform.position - Vector3.one, Vector3.one, transform.forward, Quaternion.identity, viewDistance, LayerMask.GetMask("Player"));
-    }
+        CheckSetState();
 
-    private IEnumerator Dwell()
-    {
-        canPatrol = false;
-        coroutineRunning = true;
+        //Dwell
+        if (Vector3.Distance(transform.position, patrolArea.GetChild(patrolPointIndex).position) <= agent.stoppingDistance)
+        {
+            if (Vector3.Distance(transform.position, generator.position) <= checkRadius)
+            {
+                state = State.Checking;
+            }
 
-        yield return new WaitForSecondsRealtime(dwellDuration);
+            yield return new WaitForSecondsRealtime(dwellDuration);
+        }
 
-        canPatrol = true;
-        coroutineRunning = false;
+        agent.SetDestination(patrolArea.GetChild(GetTargetPatrolPointIndex()).position);
     }
 
     void OnDrawGizmosSelected()
@@ -134,5 +215,8 @@ public class EnemyAI : MonoBehaviour
 
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, chaseRadius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
     }
 }
