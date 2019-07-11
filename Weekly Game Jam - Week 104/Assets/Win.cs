3088fdf9dcbd0551378974fd75884252614f9a47
ioane37 <feiqrishviliioane@gmail.com>
2019-07-11 17:52:56 +0400

    Updates!

--- Weekly Game Jam - Week 104/Assets/Win.cs
+++ Weekly Game Jam - Week 104/Assets/Win.cs
@@ -12,6 +12,8 @@ public class Win : MonoBehaviour
             winUI.SetActive(true);
 
             other.GetComponent<PlayerController>().enabled = false;
+
+            AudioManager.Instance.ChangeMusic(AudioManager.SoundType.Background);
         }
     }
 }
