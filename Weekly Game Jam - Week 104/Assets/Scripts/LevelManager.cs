f50956fe5e57cf9d0a14009ca8c75b831968098e
ioane37 <feiqrishviliioane@gmail.com>
2019-07-11 12:18:33 +0400

    Added Async Loading And Quit Game!

---
+++ Weekly Game Jam - Week 104/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+namespace Main.Levels
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] GameObject sliderHolder = null;
+
+        [SerializeField] Slider slider;
+        [SerializeField] TextMeshProUGUI progressText;
+
+        private bool loading = false;
+
+        public void LoadLevelAsynchronously(string levelToLoad)
+        {
+            sliderHolder.SetActive(true);
+            StartCoroutine(LoadAsynchronously(levelToLoad));
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        
+        private IEnumerator LoadAsynchronously(string levelToLoad)
+        {
+            if (!loading)
+            {
+                AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+
+                slider.gameObject.SetActive(true);
+
+                loading = true;
+
+                while (!operation.isDone)
+                {
+                    float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+                    slider.value = progress;
+                    progressText.text = "Loading: " + (progress * 100f).ToString("f2") + "%";
+
+                    yield return null;
+                }
+            }
+        }
+    }
+}
