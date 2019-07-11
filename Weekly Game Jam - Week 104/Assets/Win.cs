d1656bc495545041178ed2f22ec753ac18f74b59
ioane37 <feiqrishviliioane@gmail.com>
2019-07-11 17:43:03 +0400

    Added Win Condition!

---
+++ Weekly Game Jam - Week 104/Assets/Win.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    [SerializeField] GameObject winUI = null;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winUI.SetActive(true);
+
+            other.GetComponent<PlayerController>().enabled = false;
+        }
+    }
+}
